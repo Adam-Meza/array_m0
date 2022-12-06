27bc400a7c7849cf2ef93ad7ac3916cffc3a2594
Adam Meza <Adammeza95@gmail.com>
2022-12-06 13:43:50 -0500

    Finished  log

--- array.js
+++ array.js
@@ -1,6 +1,28 @@
 // For this project I organized Tyler the Creators albums by Title, Year, and wether or not it won the Grammy got Best Rap Album of the year.
 
-var albums = ["Goblin", "Wolf", "Cherry Bomb", "Flower Boy", "IGOR", "CMYGL"]
-var year = [2011, 2013, 2015, 2017, 2019, 2021]
-var grammys = [false, false, false, false, true, true]
+var albums = ["Goblin", "Wolf", "Cherry Bomb", "Flower Boy", "IGOR", "CMYGL"];
+var years = [2011, 2013, 2015, 2017, 2019, 2021];
+var grammys = [false, false, false, false, true, true];
 
+// If user goes to Tylers spotify they'll promote his most recent album, which they might do like:
+
+console.log(years.at(-1));
+console.log("Tyler, the Creator's most recent album", (albums.at (-1)), "was realeased in", (years.at (-1)))
+
+// If user asks which albums won a grammy it might work something like
+
+console.log("Tylers albums", albums.slice(4,6), "won Grammys");
+
+//IF tyler makes a new album here refered to as "NEW" than they would add it to his discography like this
+
+albums.push("NEW");
+console.log("Check out Tyler, the Creator's new album:", (albums.pop()));
+
+// If user was to ask if a certain albums, (in this case Cherry Bomb) won a grammy it might look like this
+var specific = 2
+console.log("Did", albums.at(specific), "win a grammy?")
+
+if (grammys.at(specific)) {
+    console.log("Yes it did!");
+} else {
+    console.log("No it didn't");
