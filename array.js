599d7153b29eafc142474c2beb40704055f4fe25
Adam Meza <Adammeza95@gmail.com>
2022-12-06 12:37:04 -0500

    Establish variables

---
+++ array.js
@@ -0,0 +1,6 @@
+// For this project I organized Tyler the Creators albums by Title, Year, and wether or not it won the Grammy got Best Rap Album of the year.
+
+var albums = ["Goblin", "Wolf", "Cherry Bomb", "Flower Boy", "IGOR", "CMYGL"]
+var year = [2011, 2013, 2015, 2017, 2019, 2021]
+var grammys = [false, false, false, false, true, true]
+
