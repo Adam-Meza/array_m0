ded27b93cae74b3db4b6da8214d1618ac1e5a877
Adam Meza <Adammeza95@gmail.com>
2022-12-07 08:26:48 -0500

    Fix formatting

--- array.js
+++ array.js
@@ -1,24 +1,27 @@
-// For this project I organized Tyler the Creators albums by Title, Year, and wether or not it won the Grammy got Best Rap Album of the year.
+// For this project I organized Tyler the Creators albums by title, year, and whether or not it won the Grammy for Best Rap Album of the year:
 
 var albums = ["Goblin", "Wolf", "Cherry Bomb", "Flower Boy", "IGOR", "CALL ME IF YOU GET LOST"];
 var years = [2011, 2013, 2015, 2017, 2019, 2021];
 var grammys = [false, false, false, false, true, true];
 
-// If user goes to Tylers spotify they'll promote his most recent album, which they might do like:
+// This console logs his most recent album, and when it was relased:
 
 console.log(years.at(-1));
 console.log("Tyler, the Creator's most recent album", (albums.at (-1)), "was realeased in", (years.at (-1)))
 
-// If user asks which albums won a grammy it might work something like
+// This console logs the names of the albums that won Grammys:
 
 console.log("Tylers albums", albums.slice(4,6), "won Grammys");
 
-//IF tyler makes a new album (which we will call "NEW") than they would add it to his discography like this
+//This pushes a new album to his discography:
 
 albums.push("NEW");
 console.log("Check out Tyler, the Creator's new album:", (albums.pop()));
 
-// This is my open ended part - If user was to ask if a certain albums, (in this case Cherry Bomb) won a grammy it might look like this
+// This is my open ended part 
+
+// This console logs whether a certain album (in this case Cherry Bomb) won a grammy it might look like this:
+
 var specific = 2
 console.log("Did", albums.at(specific), "win a grammy?")
 
@@ -28,17 +31,19 @@ if (grammys.at(specific)) {
     console.log("No it didn't");
 }
 
-// EXTRA added after reading about For statements
+// EXTRA, added after reading about For statements.
+
+//This console logs all his projects and the year they were released. 
 
 for (var x = 0; x < albums.length; x++) {
-    console.log(albums.at (x), "was released in", years.at(x));
+    console.log(albums.at (x), "was released in", years.at(x));}
 
+//This console logs whether or not a project won a Grammy or not.
 
-// couldn't get this code to work. going to focus on out standing hw. assuming im missing something about nesting
-
-} /*if (grammys.at (x)){
+for (x = 0; x < grammys.length; x++){
+    if (grammys.at (x)) {
     console.log(albums.at (x), "won a grammy!");
 } else {
-    console.log(albums.at (x)), "didn't win a grammy. :("
-}
-*/  
+    console.log(albums.at (x), "didn't win a grammy. :(");
+}}
+
